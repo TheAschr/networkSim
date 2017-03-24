8dbebfe40c05321150568a5c0196e02d15f2371c
TheAschr <schrimpf.alex@gmail.com>
2017-03-24 12:14:49 -0500

    added graphing

---
+++ graph.cpp
@@ -0,0 +1,82 @@
+#include "graph.h"
+
+Graph::Graph(Shader& shader):
+m_shader(&shader)
+{
+
+	// glGenVertexArrays(1, &m_vao);
+	// glGenBuffers(1, &m_vbo);
+
+	// glBindVertexArray(m_vao);
+
+	// glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	// //glBufferData(GL_ARRAY_BUFFER,m_buffer.size() * sizeof(GLfloat), &m_buffer[0], GL_STATIC_DRAW);
+
+	// glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	// glEnableVertexAttribArray(0);
+
+	// glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	// glBindVertexArray(0);
+
+	glGenVertexArrays(1, &m_vao);
+	glGenBuffers(1, & m_vbo);
+	//glBindVertexArray(m_vao);
+
+	//glBindBuffer(GL_ARRAY_BUFFER,0);
+	//glBindVertexArray(0);
+
+	m_verts =0;
+
+}
+
+void Graph::buffer(){
+	m_buffer.clear();
+	m_verts = 0;
+	for(int i = 0; i < m_functions.size();i++){
+		for(int j= 0; j < m_functions[i].m_lines.size();j++){
+			m_buffer.push_back(m_functions[i].m_lines[j].m_p1.x);
+			m_buffer.push_back(m_functions[i].m_lines[j].m_p1.y);
+
+			m_verts++;
+			m_buffer.push_back(m_functions[i].m_lines[j].m_p2.x);
+			m_buffer.push_back(m_functions[i].m_lines[j].m_p2.y);
+			
+			// m_buffer.push_back(m_functions[i].m_color.x);
+			// m_buffer.push_back(m_functions[i].m_color.y);
+			// m_buffer.push_back(m_functions[i].m_color.z);
+			m_verts++;
+		}
+
+	}
+
+
+	//glGenVertexArrays(1, &m_vao);
+	//glGenBuffers(1, &m_vbo);
+
+	glBindVertexArray(m_vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glBufferData(GL_ARRAY_BUFFER,m_buffer.size() * sizeof(GLfloat), &m_buffer[0], GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	glBindVertexArray(0);
+}
+
+void Graph::draw(){
+
+
+	//std::cout << m_buffer.size() << std::endl;
+
+	m_shader->Use();
+
+	glBindVertexArray(m_vao);
+	glDrawArrays(GL_LINES, 0, m_verts);
+	//std::cout << "Test" << std::endl;
+	glBindVertexArray(0);
+
+}
---
+++ graph.h
@@ -0,0 +1,73 @@
+#ifndef graph_h
+#define graph_h
+
+#include <vector>
+
+// GLEW
+//#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include "shader.h"
+
+#include "glm/glm.hpp"
+
+struct Line
+{
+	Line(glm::vec2 p1, glm::vec2 p2) : m_p1(p1), m_p2(p2){}
+	glm::vec2 m_p1;
+	glm::vec2 m_p2;
+};
+
+struct Function
+{
+	Function(GLuint id,glm::vec3 color){
+		m_id = id;
+		m_color = color;
+	}
+	std::vector<Line> m_lines;
+	glm::vec3 m_color;
+	GLuint m_id;
+
+};
+
+class Graph{
+	public:
+		Graph(Shader& shader);
+		~Graph(){
+
+		}
+
+		void addFunction(Function& function){
+			m_functions.push_back(function);
+		}
+
+		void resetFunction(GLuint id){
+			for(int i = 0; i < m_functions.size();i++)
+				if(m_functions[i].m_id == id)
+					m_functions[i].m_lines.clear();
+			buffer();
+		}
+
+		void addLine(Line& line,GLuint id){
+			for(int i = 0; i < m_functions.size();i++)
+				if(m_functions[i].m_id == id)
+					m_functions[i].m_lines.push_back(line);
+		}
+
+		void draw();
+		void buffer();
+
+	private:
+		std::vector<Function> m_functions;
+		std::vector<GLfloat> m_buffer;
+		GLuint m_vao;
+		GLuint m_vbo;
+		GLuint m_verts;
+
+		Shader* m_shader;
+};
+
+#endif
